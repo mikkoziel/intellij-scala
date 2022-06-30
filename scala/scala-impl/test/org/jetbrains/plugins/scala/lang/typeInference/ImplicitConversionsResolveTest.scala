8e52256f3db190df337b579dc3e94a8bde7da93e
Andrey.Sugak <andrey.sugak@jetbrains.com>
2022-06-30 16:16:35 +0200

    Fix typing of ScGenericCalls with propagated type parameters
    #SCL-20378 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/ImplicitConversionsResolveTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/ImplicitConversionsResolveTest.scala
@@ -34,4 +34,29 @@ class ImplicitConversionsResolveTest extends ScalaLightCodeInsightFixtureTestAda
       |}
       |""".stripMargin
   )
+
+  def testSCL20378(): Unit = checkTextHasNoErrors(
+    """
+      |import Conversions._
+      |
+      |object Test {
+      |  def main(args: Array[String]): Unit = {
+      |    println {
+      |      1.convert[String](10.0)
+      |    }
+      |  }
+      |}
+      |
+      |object Conversions {
+      |
+      |  implicit class GenericConversion2[A, B](x: A) {
+      |    def convert[R](y: B)(implicit f: (A, B) => R): R = f(x, y)
+      |  }
+      |
+      |  implicit val intToStringM: (Int, Double) => String = (x, y) => {
+      |      (y + x).toString
+      |    }
+      |}
+      |""".stripMargin
+  )
 }
